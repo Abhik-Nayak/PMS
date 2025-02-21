57f16b062fdb6392e508b9498bf067703784f13b
Abhik Nayak <abhiknayakabhik@gmail.com>
2025-02-21 09:44:35 +0530

    dark mode and white mode toggle setup done

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import { Menu, Moon, Search, Settings, Sun, User } from "lucide-react";
+import { Menu, MenuIcon, Moon, MoonIcon, Search, Settings, Sun, SunIcon, User } from "lucide-react";
 import Link from 'next/link';
+import { useAppDispatch, useAppSelector } from '@/app/redux';
+import { setIsDarkMode, setIsSidebarCollapsed } from '@/state';
 
 const index = () => {
+    const dispatch = useAppDispatch();
+    const isSidebarCollapsed = useAppSelector(
+        (state) => state.global.isSidebarCollapsed,
+    );
+    const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+    console.log(isSidebarCollapsed)
+
     return (
         <div className="flex items-center justify-between bg-white px-4 py-3 dark:bg-black">
             {/* Search Bar */}
             <div className="flex items-center gap-8">
+                {isSidebarCollapsed && <button onClick={() => dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))}>
+                    <MenuIcon className='h-8 w-8 dark:text-white' />
+                </button>}
                 <div className="relative flex h-min w-[200px]">
                     <Search className="absolute left-[4px] top-1/2 mr-2 h-5 w-5 -translate-y-1/2 transform cursor-pointer dark:text-white" />
                     <input
@@ -19,10 +32,17 @@ const index = () => {
 
             {/* Icons */}
             <div className="flex items-center">
+                <button
+                    onClick={() => dispatch(setIsDarkMode(!isDarkMode))}
+                    className={
+                        isDarkMode
+                            ? `rounded p-2 dark:hover:bg-gray-700`
+                            : `rounded p-2 hover:bg-gray-100`
+                    }>{isDarkMode ? <SunIcon /> : <MoonIcon />}</button>
                 <Link
                     href="/settings"
                     className={
-                        true
+                        isDarkMode
                             ? `h-min w-min rounded p-2 dark:hover:bg-gray-700`
                             : `h-min w-min rounded p-2 hover:bg-gray-100`
                     }
--- client/src/state/index.tsx
+++ client/src/state/index.tsx
@@ -1,4 +1,28 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-const initialState = {};
+export interface initialStateTypes {
+    isSidebarCollapsed: boolean;
+    isDarkMode: boolean;
+}
+
+const initialState: initialStateTypes = {
+    isSidebarCollapsed: false,
+    isDarkMode: false
+};
+
+export const globalSlice = createSlice({
+    name: "global",
+    initialState,
+    reducers: {
+        setIsSidebarCollapsed: (state, action: PayloadAction<boolean>) => {
+            state.isSidebarCollapsed = action.payload;
+        },
+        setIsDarkMode: (state, action: PayloadAction<boolean>) => {
+            state.isDarkMode = action.payload;
+        },
+    },
+});
+
+export const { setIsSidebarCollapsed, setIsDarkMode} = globalSlice.actions;
+export default globalSlice.reducer;
 
